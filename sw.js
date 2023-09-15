f54065525fffb0af2b3f846f2427d90a17c9e3c9
“BDTA-zky <2793217027@qq.com>
2023-09-15 17:10:39 +0800

    Site updated: 2023-09-15 17:10:39

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","740268953cdf8f38c0908671c5e61315"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
